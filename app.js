92a3dde13d3a8ebda5d7f5c92777f0852edc613c
Karol <konieczko.karol@gmail.com>
2018-07-30 19:11:57 +0200

    Game over screen + scoreboard

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-let pressedWord = ''; // Przechowywane aktualne słowo
-const words = ['jeden', 'dwa', 'trzy', 'cztery', 'piec', 'szesc', 'siedem', 'osiem', 'dziewiec']; // Tablica ze słowami
+let pressedWord = ''; 
+const words = ['jeden', 'dwa', 'trzy', 'cztery', 'piec', 'szesc', 'siedem', 'osiem', 'dziewiec'];
+const score = document.querySelector("#score");
 let points = 0;
+let losing = 0; 
+let end = 5; 
+
 
 
 window.addEventListener('keyup', (e) => {
-    if (e.key == ' ') { // Sprawdzam czy naciśnięto spacje, jeżeli tak to słowo jest zatwierdzone i sprawdzamy je dalej
+    if (e.key == ' ') { 
         console.log("SŁOWO: " + pressedWord);
         const search = document.querySelector(`.${pressedWord}`);
-        if (search != null) { // Słowo wystąpiło jeżeli search jest różny od null 
-            search.remove();// Usuwam słowo 
+        if (search != null) {
+            search.remove();
             points++;
             losing--;
+            score.textContent = `Punkty: ${points}`;
         }
-        pressedWord = ''; // Zerowanie słowa
-    } else { // To występuje, jeżeli nie wciśneliśmy spacji. Słowo nadal jest wpisywane.
+        pressedWord = ''; 
+    } else {
         pressedWord = pressedWord + e.key; 
     }
 });
@@ -23,11 +28,12 @@ window.addEventListener('keyup', (e) => {
 
 const startBtn = document.querySelector("#start");
 
-const createElement = () => { // Funkcja tworząca elementy
+const createElement = () => {
     const el = document.createElement("div");
     const randomIndex = Math.floor(Math.random() * words.length);
     el.innerText = words[randomIndex];
     el.classList.add(`${words[randomIndex]}`);
+    el.classList.add("word");
     el.style.top = Math.floor(Math.random() * 450);
     el.style.left = Math.floor(Math.random() * 450);
     const div = document.querySelector(".container");
@@ -35,23 +41,34 @@ const createElement = () => { // Funkcja tworząca elementy
     losing++;
 }
 
-
-
-let losing = 0; // Numer startowy, gdy osiągnie wartość end to gra zatrzyma się
-let end = 20; // Liczba przy której się przegrywa, jeżeli losing ją osiągnie. 
+const endGame = () => {
+    const div = document.querySelector(".container");
+    const gameOver = document.createElement("div");
+    gameOver.innerText = "GAME OVER";
+    gameOver.classList.add("gameOver");
+    
+    while (div.firstChild) {
+        div.removeChild(div.firstChild);
+    }
+    
+    div.appendChild(gameOver);
+    div.style.backgroundColor = "red";
+}
 
 
 const game = () => {
     let timer = setInterval(() => {
         console.log(losing);
-        if (false) {
+        if (losing >= end) {
             clearInterval(timer);
-        }
+            endGame();
+        } else {
         createElement();
+        }
     }, 1000);
 }
 
-startBtn.addEventListener("click", () => { // Rozpoczęcie gry po wciśnięciu przycisku
+startBtn.addEventListener("click", () => {
     game();
     console.log("odpalana jest funkcja game");
 }, {once: true});
